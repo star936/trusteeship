9fe196827f5bb261086afac5d8fa741fda9eae01
star936 <1542938982@qq.com>
2018-07-18 23:16:18 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/trusteeship'
+UPLOAD_FOLDER = '/Users/dang/work/pywork/trusteeship/upload'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
